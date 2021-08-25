5501d4200bbbda4628cdf613f0e0d30511502954
Jason <airjason13@gmail.com>
2021-08-25 17:34:59 +0800

    1. drag and drop layout pixmap imaplement

--- c_led_layout_window.py
+++ c_led_layout_window.py
@@ -13,34 +13,57 @@ log = utils.log_utils.logging_init(__file__)
 class LedLayoutWindow(QWidget):
     def __init__(self, led_wall_w, led_wall_h, cabinet_w, cabinet_h, margin):
         super(LedLayoutWindow, self).__init__()
+        self.scale_factor = 4
         self.setWindowTitle("LED Wall")
-        self.resize(led_wall_w*2 + margin*2, led_wall_h*2 + margin*2)
+        self.resize(led_wall_w*self.scale_factor + margin*2, led_wall_h*self.scale_factor + margin*2)
         self.led_wall_w = led_wall_w
         self.led_wall_h = led_wall_h
         self.cabinet_w = cabinet_w
         self.cabinet_h = cabinet_h
-        self.led_fake_label = QLabel(self)
-        self.led_fake_pixmap = utils.qtui_utils.gen_led_layout_pixmap(led_wall_w, led_wall_h, margin, Qt.GlobalColor.black, Qt.GlobalColor.white)
-        self.led_fake_label.setPixmap(self.led_fake_pixmap)
+        self.led_wall_margin = 10
 
-        self.gen_single_cabinet_label(self.cabinet_w, self.cabinet_h)
-        self.led_fake_label.setScaledContents(True)
+        '''Gen led wall layout with scalable'''
+        self.led_wall_pixel_pixmap = utils.qtui_utils.gen_led_layout_pixmap(self.led_wall_w, self.led_wall_h, self.led_wall_margin,
+                                                                           Qt.GlobalColor.black, Qt.GlobalColor.white)
+        self.led_fake_label = Droppable_led_label(self.led_wall_pixel_pixmap, self)
+        print("led_fake_label label width", self.led_fake_label.width())
+        print("led_fake_label label height", self.led_fake_label.height())
 
+        self.single_cabinet_label = self.gen_single_cabinet_label(self.cabinet_w, self.cabinet_h, 0, 0)
+        self.led_fake_label.set_drag_label(self.single_cabinet_label)
+        print("single_cabinet_label label width", self.single_cabinet_label.width())
+        print("single_cabinet_label label height", self.single_cabinet_label.height())
 
+        '''drag_label_ori_offset 為drag_label內之座標, 為QPoint'''
+        self.drag_label_ori_offset = None
+        self.drag_label_ori_pos = None
+        self.drag_label = None
+        self.single_cabinet_label.start_drag_signal.connect(self.start_drag)
 
-    #def resize(self, a0: QSize) -> None:
+    def start_drag(self, drag_label, start_offset):
+        log.debug('start_drag')
+        self.drag_label = drag_label
+        self.drag_label_ori_pos = self.drag_label.pos()
+        ##self.drag_label.move(start_offset)
+        print(self.drag_label_ori_pos)
+        self.drag_label_ori_offset = start_offset
+        print(start_offset)
+
     def resizeEvent(self, a0: QResizeEvent) -> None:
         self.led_fake_label.resize(int(self.width()), int(self.height()))
         a0.accept()
 
     def wheelEvent(self, a0: QWheelEvent) -> None:
+        #scale_factor = 1
         if a0.angleDelta().y() > 0:
-            self.resize(int(self.width()*1.1), int(self.height()*1.1))
+            scale_factor = 1.1
         else :
-            self.resize(int(self.width() * 0.9), int(self.height() * 0.9))
+            scale_factor = 0.9
+        self.resize(int(self.width() * scale_factor), int(self.height() * scale_factor))
+        self.single_cabinet_label.resize(int(self.single_cabinet_label.width() * scale_factor),
+                                         int(self.single_cabinet_label.height() * scale_factor))
         self.led_fake_label.resize(int(self.width()), int(self.height()))
 
-        self.single_cabinet_label.resize((int(self.width())*(self.cabinet_w/self.led_wall_w)), int(self.height())*(self.cabinet_h/self.led_wall_h))
         log.debug("width : %d", self.width())
         log.debug("height : %d", self.height())
         a0.accept()
@@ -48,14 +71,76 @@ class LedLayoutWindow(QWidget):
     def change_led_wall_res(self, led_wall_w, led_wall_h, margin):
         log.debug("")
         self.resize(led_wall_w * 2 + margin * 2, led_wall_h * 2 + margin * 2)
-        self.led_fake_pixmap = utils.qtui_utils.gen_led_layout_pixmap(led_wall_w, led_wall_h, margin, Qt.GlobalColor.black, Qt.GlobalColor.white)
+        self.led_fake_pixmap = utils.qtui_utils.gen_led_layout_pixmap(led_wall_w, led_wall_h, margin,
+                                                                      Qt.GlobalColor.black, Qt.GlobalColor.white)
         self.led_fake_label.setPixmap(self.led_fake_pixmap)
         self.led_fake_label.resize(int(self.width()), int(self.height()))
 
-    def gen_single_cabinet_label(self, cabinet_w, cabinet_h):
+    def gen_single_cabinet_label(self, cabinet_w, cabinet_h, start_x, start_y):
         self.single_cabinet_pixmap = utils.qtui_utils.gen_led_cabinet_pixmap(cabinet_w, cabinet_h, 0,
-                                                                             Qt.GlobalColor.transparent, Qt.GlobalColor.blue, 0x80, 0)
-        self.single_cabinet_label = QLabel(self)
-        self.single_cabinet_label.setPixmap(self.single_cabinet_pixmap)
-        #self.single_cabinet_label.setStyleSheet("background-color : rgba(0,0,0,90%)")
-        self.single_cabinet_label.setScaledContents(True)
+                                                                             Qt.GlobalColor.transparent, Qt.GlobalColor.red, 0x80, 0)
+        return Draggable_cabinet_label(self, self.single_cabinet_pixmap, start_x, start_y)
+
+
+class Draggable_cabinet_label(QLabel):
+    start_drag_signal = pyqtSignal(QLabel, QPoint)
+    def __init__(self, parent, image, start_x, start_y):
+        super(QLabel, self).__init__(parent)
+        self.setPixmap(QPixmap(image))
+        self.setScaledContents(True)
+        self.move(start_x, start_y)
+        self.show()
+
+    def mousePressEvent(self, event):
+        log.debug('mousePressEvent')
+        if event.button() == Qt.LeftButton:
+            self.drag_start_position = event.pos()
+            self.start_drag_signal.emit(self, self.drag_start_position)
+
+
+    def mouseMoveEvent(self, event):
+        log.debug('mouseMoveEvent')
+        if not (event.buttons() & Qt.LeftButton):
+            return
+        if (event.pos() - self.drag_start_position).manhattanLength() < QApplication.startDragDistance():
+            return
+
+        drag = QDrag(self)
+        mimedata = QMimeData()
+        mimedata.setText(self.text())
+        mimedata.setImageData(self.pixmap().toImage())
+
+        drag.setMimeData(mimedata)
+        pixmap = QPixmap(self.size())
+        painter = QPainter(pixmap)
+        painter.drawPixmap(self.rect(), self.grab())
+        painter.end()
+        drag.setPixmap(pixmap)
+        drag.setHotSpot(event.pos())
+        drag.exec_(Qt.CopyAction | Qt.MoveAction)
+
+class Droppable_led_label(QLabel):
+    def __init__(self, image, parent):
+        super().__init__( parent)
+
+        self.setPixmap(QPixmap(image))
+        self.setScaledContents(True)
+        self.setAcceptDrops(True)
+
+
+
+
+    def dragEnterEvent(self, event):
+        log.debug("dragEnterEvent")
+        if event.mimeData().hasImage():
+            event.acceptProposedAction()
+
+    def set_drag_label(self, drag_label):
+        self.drag_label = drag_label
+
+    def dropEvent(self, event):
+        log.debug("dropEvent")
+        pos = event.pos()
+        #改成用pyqtsignal處理可能比較好
+        self.drag_label.move(pos)
+        event.acceptProposedAction()
--- utils/qtui_utils.py
+++ utils/qtui_utils.py
@@ -17,6 +17,7 @@ def gen_led_layout_type_pixmap( led_w, led_h, margin, layout_type):
     '''draw line'''
     if layout_type == 0:
         for i in range(max_line):
+
             pixmap_paint.drawLine(margin, margin*(i+1), led_w + margin , margin*(i+1))
             if i % 2 == 0:
                 if i == (max_line - 1):
@@ -35,53 +36,91 @@ def gen_led_layout_type_pixmap( led_w, led_h, margin, layout_type):
                     pass
                 else:
                     pixmap_paint.drawLine(margin, margin*(i+1), margin, margin*(i+2))
+
     return pixmap_led_layout_type
 
 def gen_led_layout_pixmap(led_w, led_h, margin, bg_color, point_color):
-    pixmap_led_layout = QPixmap(led_w*2 + margin * 2, led_h*2 + margin * 2)
+    scale_factor = 8
+    pixmap_led_layout = QPixmap(led_w*scale_factor + margin * 2, led_h*scale_factor + margin * 2)
     #pixmap_led_layout.fill(Qt.GlobalColor.white)
     pixmap_led_layout.fill(bg_color)
     pixmap_paint = QPainter(pixmap_led_layout)
     #pixmap_paint.setPen(Qt.GlobalColor.red)
     pixmap_paint.setPen(point_color)
-    for h in range(led_h*2):
-        for w in range(led_w*2):
-            if w%2 == 1:
+    for h in range(led_h*scale_factor):
+        for w in range(led_w*scale_factor):
+            if w%scale_factor != 0:
                 continue
-            if h%2 == 1:
+            if h%scale_factor != 0:
                 continue
             pixmap_paint.drawPoint(margin + w, margin + h)
-
+    log.debug("pixmap_led_layout width : %d", pixmap_led_layout.width())
+    log.debug("pixmap_led_layout height : %d", pixmap_led_layout.height())
     return pixmap_led_layout
 
-def gen_led_cabinet_pixmap(led_w, led_h, margin, bg_color, point_color, alpha, layout_type):
+''' scale factor is 4'''
+''' not used now'''
+def gen_led_cabinet_pixmap_x2(led_w, led_h, margin, bg_color, point_color, alpha, layout_type):
+    log.debug('')
     '''margin is 0 pixel, left/right/up/bottom is 0 pixel'''
+    arrow_width = 1
     pixmap_led_layout_type = QPixmap(led_w*2 , led_h*2 )
     pixmap_led_layout_type.fill(bg_color)
     pixmap_paint = QPainter(pixmap_led_layout_type)
     pixmap_paint.setPen(point_color)
     if margin == 0:
-        max_line = led_h*2
+        max_line = led_h*4
     else:
         max_line = int(led_h*2 / margin) + 1
-    '''draw line'''
+    
+
     if layout_type == 0:
-        for i in range(max_line):
-            if i % 4 == 0 or i % 4 == 2:
+        for i in range( max_line ):
+            if i % 4 == 0 : #or i % 4 == 2:
                 pixmap_paint.drawLine(margin, margin + i , led_w*2 + margin, margin + i)
-            elif i % 4 == 1:
-                pixmap_paint.drawLine(margin, margin + (i ), margin, margin + (i + 1))
-            elif i % 4 == 3:
-                pixmap_paint.drawLine(margin+ led_w*2-1, margin + (i), margin + led_w*2-1, margin + (i + 1))
-            
+            #elif i % 4 == 1:
+            #    pixmap_paint.drawLine(margin, margin + (i), margin, margin + (i + 2))
+            #elif i % 4 == 3:
+            #    pixmap_paint.drawLine(margin+ led_w*2-1, margin + (i), margin + led_w*2-1, margin + (i + 2))'''
+
+''' scale factor is 4'''
+def gen_led_cabinet_pixmap(led_w, led_h, margin, bg_color, point_color, alpha, layout_type):
+    '''margin is 0 pixel, left/right/up/bottom is 0 pixel'''
+    arrow_width = 1
+    pixmap_led_layout_type = QPixmap(led_w*4 , led_h*4 )
+    pixmap_led_layout_type.fill(bg_color)
+    pixmap_paint = QPainter(pixmap_led_layout_type)
+    pixmap_paint.setPen(point_color)
+    if margin == 0:
+        max_line = led_h*4
+    else:
+        max_line = int(led_h*4 / margin) + 1
+    '''draw line'''
+    h_drawed = 0
+    if layout_type == 0:
+        for i in range( max_line):
+            if i == max_line:
+                log.debug("max line")
+            if i == 0:
+                continue
+            if i % 8 == 1 or i % 8 == 5:
+                pixmap_paint.drawLine(margin, margin + i , led_w*4 + margin, margin + i)
+                h_drawed += 1
+                if h_drawed >= led_h:
+                    break
+            elif i % 8 == 0 or i % 8 == 6 or i % 8 == 7 :
+                pixmap_paint.drawLine(margin, margin + (i), margin, margin + (i + 1))
+            elif i % 8 == 2 or i % 8 == 3 or i % 8 == 4:
+                pixmap_paint.drawLine(margin + led_w * 4 , margin + (i), margin + led_w * 4 , margin + (i + 1))
     elif layout_type == 1:
         for i in range(max_line):
-            pixmap_paint.drawLine(margin, margin * (i + 1), led_w + margin, margin * (i + 1))
-            if i % 2 == 1:
-                pixmap_paint.drawLine(margin + led_w, margin * (i + 1), margin + led_w, margin * (i + 2))
-            else:
-                if i == (max_line - 1):
-                    pass
-                else:
-                    pixmap_paint.drawLine(margin, margin * (i + 1), margin, margin * (i + 2))
+            if i % 4 == 0 or i % 4 == 2:
+                pixmap_paint.drawLine(margin, margin + i, led_w * 2 + margin, margin + i)
+            elif i % 4 == 1:
+                pixmap_paint.drawLine(margin + led_w * 2 - 1, margin + (i), margin + led_w * 2 - 1, margin + (i + 1))
+            elif i % 4 == 3:
+                pixmap_paint.drawLine(margin, margin + (i), margin, margin + (i + 1))
+
+
+
     return pixmap_led_layout_type
