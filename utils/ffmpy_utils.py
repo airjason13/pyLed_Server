b82b0aed665fee39813dfdacc4f465b50fc284e7
airjason13 <airjason13@gmail.com>
2021-11-18 14:45:06 +0800

    1. fix mp4 and jpg streaming pix_fmt

--- utils/ffmpy_utils.py
+++ utils/ffmpy_utils.py
@@ -36,7 +36,7 @@ def neo_ffmpy_execute(video_path, brightness, contrast, red_bias, green_bias, bl
     # add TEXT
     if "blank.mp4" in video_path:
         drawtext_str = "drawtext=fontfile=" + internal_media_folder + \
-                       "/fonts/msjhbd.ttc:text='歡迎MIH蒞臨指導':x=10*w/80-80*t:y=0:fontsize=24*h/96:fontcolor=white"
+                       "/fonts/msjhbd.ttc:text='歡迎MIH蒞臨指導':x=10*w/80-80*t:y=0:fontsize=96*h/96:fontcolor=white"
         filter_params = "zmq," + eq_str + "," + color_level_str + "," + drawtext_str + "," + scale_params
     else:
         filter_params = "zmq," + eq_str + "," + color_level_str + "," + scale_params
@@ -79,8 +79,8 @@ def neo_ffmpy_execute(video_path, brightness, contrast, red_bias, green_bias, bl
                 inputs={video_path: ["-re"]},
 
                 outputs={
-                    udp_sink: ["-preset", "ultrafast", "-vcodec", "libx264", '-filter_complex', filter_params, "-f",
-                               "h264", "-localaddr", "192.168.0.2"],
+                    udp_sink: ["-preset", "ultrafast", "-vcodec", "libx264", '-filter_complex', filter_params,
+                               "-g", "120", "-f", "h264", "-pix_fmt", "yuv420p", "-localaddr", "192.168.0.2"],
                 }
             )
         elif video_path.endswith("jpg") or video_path.endswith("png"):
