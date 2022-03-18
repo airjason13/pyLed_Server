152d0e0990189eff97101d42bf79260477887b79
airjason13 <airjason13@gmail.com>
2022-03-18 15:04:07 +0800

    1. fix webp generate method, try 3 times to generate webp thumbnail.

--- utils/ffmpy_utils.py
+++ utils/ffmpy_utils.py
@@ -448,6 +448,8 @@ def neo_ffmpy_cast_video_depreciated(video_path, cast_dst_0, cast_dst_1, width=8
 
 def gen_webp_from_video(file_folder, video):
     # use hashlib md5 to generate preview file name
+    error_count = 0
+    error_count_threshold = 3
     video_name = video.split(".")[0]
     video_extension = video.split(".")[1]
     # log.debug("video_extension = %s", video_extension)
@@ -456,37 +458,48 @@ def gen_webp_from_video(file_folder, video):
     # thumbnail_path = internal_media_folder + ThumbnailFileFolder + video.replace(".mp4", ".webp")
     thumbnail_path = internal_media_folder + ThumbnailFileFolder + preview_file_name + ".webp"
     video_path = file_folder + "/" + video
-    # log.debug("video_path = %s", video_path)
-    # log.debug("thumbnail_path = %s", thumbnail_path)
+    log.debug("video_path = %s", video_path)
+    log.debug("thumbnail_path = %s", thumbnail_path)
     thunbnail_folder_path = internal_media_folder + ThumbnailFileFolder
     if not os.path.exists(thunbnail_folder_path):
         os.makedirs(thunbnail_folder_path)
-    try:
-        if os.path.isfile(thumbnail_path) is False:
-            global_opts = '-hide_banner -loglevel error'
-            if video_extension in ["jpeg", "jpg", "png"]:
-                # log.debug("still image")
-                ff = ffmpy.FFmpeg(
-                    global_options=global_opts,
-                    inputs={video_path: ['-loop', str(still_image_loop_cnt), '-t', str(preview_period)]},
-                    outputs={thumbnail_path: ['-vf', 'scale=320:240']}
-                )
-            else:
-                ff = ffmpy.FFmpeg(
-                    global_options=global_opts,
-                    inputs={video_path: ['-ss', str(preview_start_time), '-t', str(preview_period)]},
-                    outputs={thumbnail_path: ['-vf', 'scale=320:240']}
-                )
-            # log.debug("%s", ff.cmd)
-            ff.run()
-    except Exception as e:
-        log.debug(e)
+    while True:
+        try:
+            if os.path.isfile(thumbnail_path) is False:
+                global_opts = '-hide_banner -loglevel error'
+                if video_extension in ["jpeg", "jpg", "png"]:
+                    # log.debug("still image")
+                    ff = ffmpy.FFmpeg(
+                        global_options=global_opts,
+                        inputs={video_path: ['-loop', str(still_image_loop_cnt), '-t', str(preview_period)]},
+                        outputs={thumbnail_path: ['-vf', 'scale=320:240']}
+                    )
+                else:
+                    ff = ffmpy.FFmpeg(
+                        global_options=global_opts,
+                        inputs={video_path: ['-ss', str(preview_start_time), '-t', str(preview_period)]},
+                        outputs={thumbnail_path: ['-vf', 'scale=320:240']}
+                    )
+                # log.debug("%s", ff.cmd)
+                ff.run()
+        except Exception as e:
+            log.debug("Excception %s", e)
+            os.remove(thumbnail_path)
+            error_count += 1
+            if error_count > error_count_threshold:
+                log.debug("source file : %s might be fault", video_path)
+                break
+            continue
+        break
+    log.debug("%s generated good", thumbnail_path)
     return thumbnail_path
+
 
 def gen_webp_from_video_threading(file_folder, video):
     threads = []
     threads.append(threading.Thread(target=gen_webp_from_video, args=(file_folder, video,)))
     threads[0].start()
+
 
 def ffmpy_set_video_param_level(param_name, level):
     cmd = ""
