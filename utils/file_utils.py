084f18c5c169e666d59cd8187dc4d977bb8b70b2
airjason13 <airjason13@gmail.com>
2022-03-31 14:45:31 +0800

    1. fix pen drive mounted but not scan 2. revision to 22033003

--- utils/file_utils.py
+++ utils/file_utils.py
@@ -27,6 +27,7 @@ def get_mount_points(devices=None):
     context = pyudev.Context()
 
     mount_points = []
+    # 分割磁區的pen drive
     removable = [device for device in context.list_devices(subsystem='block', DEVTYPE='disk') if
                  device.attributes.asstring('removable') == "1"]
     for device in removable:
@@ -39,6 +40,16 @@ def get_mount_points(devices=None):
                 #print("  {}: {}".format(p.device, p.mountpoint))
                 mount_points.append(p.mountpoint)
 
+    # 未分割磁區的pen drive
+    for device in removable:
+        partitions = [device.device_node for device in
+                      context.list_devices(subsystem='block', DEVTYPE='disk', parent=device)]
+        #print("All removable partitions: {}".format(", ".join(partitions)))
+        #print("Mounted removable partitions:")
+        for p in psutil.disk_partitions():
+            if p.device in partitions:
+                #print("  {}: {}".format(p.device, p.mountpoint))
+                mount_points.append(p.mountpoint)
     return mount_points
 
 
