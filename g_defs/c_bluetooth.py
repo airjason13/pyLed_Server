f1c2d417d59149860a475024086ef457b16c2bab
airjason13 <airjason13@gmail.com>
2022-07-18 11:19:27 +0800

    1. test bt

--- g_defs/c_bluetooth.py
+++ g_defs/c_bluetooth.py
@@ -16,20 +16,28 @@ class BlueTooth(QtCore.QThread):
 
 	def run(self):
 		try:
-			subprocess.Popen("hciconfig hci0 sspmode 1", stdin=subprocess.PIPE, stdout=subprocess.PIPE,
+			subprocess.Popen("hciconfig hci0 sspmode 1", shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE,
 			                 stderr=subprocess.PIPE)
-			subprocess.Popen("hciconfig hci0 sspmode", stdin=subprocess.PIPE, stdout=subprocess.PIPE,
+			subprocess.Popen("hciconfig hci0 sspmode", shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE,
 			                 stderr=subprocess.PIPE)
-			subprocess.Popen("bluetoothctl discoverable on", stdin=subprocess.PIPE, stdout=subprocess.PIPE,
+			subprocess.Popen("bluetoothctl discoverable on", shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE,
 			                 stderr=subprocess.PIPE)
-			subprocess.Popen("bluetoothctl pair on", stdin=subprocess.PIPE, stdout=subprocess.PIPE,
+			subprocess.Popen("bluetoothctl pair on", shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE,
 			                 stderr=subprocess.PIPE)
-			self.bt_process = subprocess.Popen("bluetoothctl --agent=NoInputNoOutput", stdin=subprocess.PIPE, stdout=subprocess.PIPE,
+			self.bt_process = subprocess.Popen("bluetoothctl", shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE,
 			                 stderr=subprocess.PIPE)
 		except Exception as e:
 			log.debug(e)
 		try:
+			self.write(self.bt_process, "agent off")
+			log.debug(self.read(self.bt_process))
+			self.write(self.bt_process, "agent NoInputNoOutput")
+			log.debug(self.read(self.bt_process))
 			self.write(self.bt_process, "default-agent")
+			log.debug(self.read(self.bt_process))
+			self.write(self.bt_process, "discoverable on")
+			log.debug(self.read(self.bt_process))
+			self.write(self.bt_process, "pairable on")
 			log.debug(self.read(self.bt_process))
 		except Exception as e:
 			log.debug(e)
@@ -38,16 +46,16 @@ class BlueTooth(QtCore.QThread):
 			log.debug("bt running")
 			sleep(20)
 			try:
-				self.write(self.bt_process, "bluetoothctl discoverable on")
-				log.debug(self.read(self.bt_process))
-				self.write(self.bt_process, "bluetoothctl pairable on")
-				log.debug(self.read(self.bt_process))
+				readline = self.read(self.bt_process)
+				log.debug(readline)
+				if "yes/no" in readline:
+					self.write(self.bt_process, "yes")
 			except Exception as e:
 				log.debug(e)
 
-	def read(process):
+	def read(self, process):
 		return process.stdout.readline().decode("utf-8").strip()
 
-	def write(process, message):
+	def write(self, process, message):
 		process.stdin.write(f"{message.strip()}\n".encode("utf-8"))
 		process.stdin.flush()
