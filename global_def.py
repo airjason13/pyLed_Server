56c442d340263d8190ab5e075589e4752ab0b63c
airjason13 <airjason13@gmail.com>
2023-03-23 14:49:13 +0800

    1. disable play audio while playing file 2. add fps=23 3. resivion

--- global_def.py
+++ global_def.py
@@ -3,7 +3,7 @@ import platform
 import os
 from pathlib import Path
 """Software version"""
-version = "LS230217002"
+version = "LS230323001"
 
 
 def get_led_role():
@@ -184,4 +184,5 @@ web_cmd_interval = 3
 
 fps_30="30/1"
 fps_25="25/1"
-target_fps=fps_25
+fps_23="23/1"
+target_fps=fps_23
